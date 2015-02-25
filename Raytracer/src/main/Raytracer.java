b075880778b8990a63da47b42db64bb3f27bfb2c
ericandoh <ohjingyu@gmail.com>
2015-02-24 18:29:44 -0800

    made a frame lmao

--- Raytracer/src/main/Raytracer.java
+++ Raytracer/src/main/Raytracer.java
@@ -1,6 +1,12 @@
 package main;
 
+import javax.swing.JFrame;
+
 public class Raytracer {
 	
-  // ayy lmao
+	//ayy lmao
+	public static void main(String[] args) {
+		JFrame frame = new JFrame();
+		frame.show();
+	}
 }
