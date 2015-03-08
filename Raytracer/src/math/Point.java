9452be03baabeec76337872bcbc405b783a4ba56
Richard Jiang <richardjiang@berkeley.edu>
2015-03-07 17:16:05 -0800

    math

---
+++ Raytracer/src/math/Point.java
@@ -0,0 +1,7 @@
+package math;
+
+public class Point extends Vector3 {
+	public Point(float x, float y, float z) {
+		super(x, y, z);
+	}	
+}
--- Raytracer/src/math/Vector3.java
+++ Raytracer/src/math/Vector3.java
@@ -2,7 +2,17 @@ package math;
 
 public class Vector3 {
 	public float x, y, z;
-	//kek
+	
+	public Vector3() {
+		x = 0f;
+		y = 0f;
+		z = 0f;
+	}
+	public Vector3(float x, float y, float z) {
+		this.x = x;
+		this.y = y;
+		this.z = z;
+	}
 	
 	public Vector3(boolean isRandom) {
 		if (isRandom) {
