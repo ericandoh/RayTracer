0a7373bd27938e164006ec8efafdc30306740972
ericandoh <ohjingyu@gmail.com>
2015-03-07 16:28:59 -0800

    added .classpath files

--- Raytracer/src/main/Raytracer.java
+++ Raytracer/src/main/Raytracer.java
@@ -1,12 +1,16 @@
 package main;
 
 import javax.swing.JFrame;
+import javax.swing.JPanel;
 
 public class Raytracer {
 	
 	//ayy lmao
 	public static void main(String[] args) {
 		JFrame frame = new JFrame();
-		frame.show();
+		JPanel panel = new JPanel();
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		//f
+		frame.setVisible(true);
 	}
 }
