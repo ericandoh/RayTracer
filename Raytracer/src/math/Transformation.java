425609e5a23224bc5e5ac0bc34ca13babe50a9cd
ericandoh <ohjingyu@gmail.com>
2015-03-16 16:18:21 -0700

    we now read in/parse all valid command line arguments

--- Raytracer/src/math/Transformation.java
+++ Raytracer/src/math/Transformation.java
@@ -11,4 +11,17 @@ public class Transformation {
 		this.transform = transform;
 		this.inverse = inverse;
 	}
+	
+	public void translate(float tx, float ty, float tz) {
+		
+	}
+	public void rotate(float rx, float ry, float rz) {
+		
+	}
+	public void scale(float sx, float sy, float sz) {
+		
+	}
+	public void reset() {
+		
+	}
 }
