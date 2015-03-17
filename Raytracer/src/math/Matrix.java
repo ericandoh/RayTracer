d08067e4c86c0816f236f8d489eca1b1cba9c89c
ericandoh <ohjingyu@gmail.com>
2015-03-16 18:40:48 -0700

    added in matrix math/transformation math methods

--- Raytracer/src/math/Matrix.java
+++ Raytracer/src/math/Matrix.java
@@ -1,7 +1,13 @@
 package math;
 
 public class Matrix {
+	
+	private static float[][] tempMatrix = new float[4][4];
+	private static float[] tempVectorA = new float[4];
+	private static float[] tempVectorB = new float[4];
+	
 	public float[][]matrix;
+	
 	
 	public Matrix(float[][]matrix) {
 		this.matrix = new float[matrix.length][matrix[0].length];
@@ -12,8 +18,90 @@ public class Matrix {
 		}
 	}
 	public Matrix() {
-		matrix = new float[4][4];
+		this(4);
 	}
+	public Matrix(int dim) {
+		matrix = new float[dim][dim];
+	}
+	public void set(float [][] matrix) {
+		for(int i = 0; i < matrix.length; i++) {
+			for(int j = 0; j < matrix[0].length; j++) {
+				this.matrix[i][j] = matrix[i][j];
+			}
+		}
+	}
+	public void clear() {
+		for(int i = 0; i < matrix.length; i++) {
+			for(int j = 0; j < matrix[0].length; j++) {
+				this.matrix[i][j] = 0;
+			}
+		}
+	}
+	public void setIdentity() {
+		for(int i = 0; i < matrix.length; i++) {
+			for(int j = 0; j < matrix[0].length; j++) {
+				this.matrix[i][j] = (i == j) ? 1 : 0;
+			}
+		}
+	}
+	
+	public Matrix add(Matrix src, float b) {
+		for(int i = 0; i < matrix.length; i++) {
+			for(int j = 0; j < matrix[0].length; j++) {
+				src.matrix[i][j] = this.matrix[i][j] + b;
+			}
+		}
+		return src;
+	}
+	public Matrix add(Matrix src, Matrix b) {
+		for(int i = 0; i < matrix.length; i++) {
+			for(int j = 0; j < matrix[0].length; j++) {
+				src.matrix[i][j] = this.matrix[i][j] + b.matrix[i][j];
+			}
+		}
+		return src;
+	}
+	public Matrix scale(Matrix src, float b) {
+		for(int i = 0; i < matrix.length; i++) {
+			for(int j = 0; j < matrix[0].length; j++) {
+				src.matrix[i][j] = this.matrix[i][j] * b;
+			}
+		}
+		return src;
+	}
+	
+	public Point multiply(Point src, Point a) {
+		//c_i1 = A_ij*x_j1
+		tempVectorA[0] = a.x;
+		tempVectorA[1] = a.y;
+		tempVectorA[2] = a.z;
+		tempVectorA[3] = 1;
+		
+		for (int i = 0; i < this.matrix.length; i++) {
+			tempVectorB[i] = 0;
+			for (int j = 0; j < tempVectorA.length; j++) {
+				tempVectorB[i] += this.matrix[i][j] * tempVectorA[j];
+			}
+		}
+		src.x = tempVectorB[0] / tempVectorB[3];
+		src.y = tempVectorB[1] / tempVectorB[3];
+		src.z = tempVectorB[2] / tempVectorB[3];
+		return src;
+	}
+	
+	public void multiply(Matrix src, Matrix b) {
+		//c_np = a_n1 b_1p + a_n2 b_2p + ... +a_nm b_mp
+		for (int n = 0; n < this.matrix.length; n++) {
+			for (int p = 0; p < b.matrix[0].length; p++) {
+				tempMatrix[n][p] = 0;
+				for (int j = 0; j < b.matrix.length; j++) {
+					tempMatrix[n][p] += this.matrix[n][j] * b.matrix[j][p];
+				}
+			}
+		}
+		src.set(tempMatrix);
+	}
+	
 	// Determinant of 3x3 matrix
 	public float determinant() {
 		if(matrix.length == 3) {
@@ -27,9 +115,44 @@ public class Matrix {
 			return 1.0f;
 		}
 	}
+	
+	public String toString() {
+		String mat = "[";
+		for(int i = 0; i < matrix.length; i++) {
+			mat += "[";
+			for(int j = 0; j < matrix[0].length; j++) {
+				mat += this.matrix[i][j]+",";
+			}
+			if (i < matrix.length - 1)
+				mat += "]\n";
+		}
+		return mat + "]]";
+	}
+	
 	public static void main(String[]args) {
+		
+		//test determinant
 		float[][]mat = {{1f, 2f, 3f}, {0f, -4f, 1f}, {0f, 3f, -1f}};
 		Matrix matrix = new Matrix(mat);
-		System.out.print(matrix.determinant());
+		System.out.println(matrix.determinant());
+		
+		//test setIdentity
+		Matrix a = new Matrix(4);
+		a.setIdentity();
+		System.out.println(a);
+		
+		//test matrix multiplication
+		Matrix b = new Matrix(4);
+		float[][]matA = {{1, 2, 3, 4}, {0, -4, 1, 2}, {0, 3, 2, 6}, {7, 2, -4, 8}};
+		float[][]matB = {{6, 3, -2, 3}, {8, 1, 0, 6}, {-2, 4, 3, 1}, {0, -5, 7, 6}};
+		a.set(matA);
+		b.set(matB);
+		
+		Matrix c = new Matrix(4);
+		a.multiply(c, b);
+		System.out.println("Multiplying AB = ");
+		System.out.println(c);
+		//should equal {{16, -3, 35, 42}, {-34, -10, 17, -11}, {20, -19, 48, 56}, {66, -33, 30, 77}}
+		
 	}
 }
--- Raytracer/src/math/Transformation.java
+++ Raytracer/src/math/Transformation.java
@@ -3,9 +3,16 @@ package math;
 public class Transformation {
 	public Matrix transform;
 	public Matrix inverse;
+	
+	private Matrix temp = new Matrix(4);
+	private Matrix temp2 = new Matrix(4);
+	private Matrix rax = new Matrix(4);
+	private Vector3 rCarrot = new Vector3();
+	
 	public Transformation() {
 		transform = new Matrix();
 		inverse = new Matrix();
+		transform.setIdentity();
 	}
 	public Transformation(Matrix transform, Matrix inverse) {
 		this.transform = transform;
@@ -13,15 +20,98 @@ public class Transformation {
 	}
 	
 	public void translate(float tx, float ty, float tz) {
-		
+		temp.setIdentity();
+		temp.matrix[0][3] = tx;
+		temp.matrix[1][3] = ty;
+		temp.matrix[2][3] = tz;
+		//transform = transform * temp
+		transform.multiply(transform, temp);
 	}
 	public void rotate(float rx, float ry, float rz) {
+		rCarrot.set(rx, ry, rz);
+		//theta is in degrees (0-360)
+		float theta = (float)(rCarrot.magnitude() / 360  * 2 * Math.PI);
+		rCarrot.normalize(rCarrot);
+		//rodriguez formula
+		//R = ((r rt)+sin(theta)(rx)-cos(theta)(rx)(rx))
+		//OR: R = I + (sin0)K+(1-cos0)K^2
+		rax.clear();
+		rax.matrix[0][1] = -rCarrot.z;
+		rax.matrix[0][2] = rCarrot.y;
+		rax.matrix[1][0] = rCarrot.z;
+		rax.matrix[1][2] = -rCarrot.x;
+		rax.matrix[2][0] = -rCarrot.y;
+		rax.matrix[2][1] = rCarrot.x;
+		//rax.matrix[3][3] = 1;
 		
+		float sint = (float) Math.sin(theta);
+		float oneMinusCost = 1.0f - (float)(Math.cos(theta));
+		
+		//temp = I
+		temp.setIdentity();
+		//temp2 = sint*(Rx)
+		rax.scale(temp2, sint);
+		//temp = I + sint*(Rx)
+		temp.add(temp, temp2);
+		
+		//temp2 = (Rx)(Rx)
+		rax.multiply(temp2, rax);
+		//temp2 = (1-cost)*(Rx)^2
+		temp2.scale(temp2, oneMinusCost);
+		//temp = I + sint*(Rx) + (1-cost)*(Rx)^2
+		temp.add(temp, temp2);
+		
+		//transform = transform * temp
+		transform.multiply(transform, temp);
 	}
 	public void scale(float sx, float sy, float sz) {
+		temp.setIdentity();
+		temp.matrix[0][0] = sx;
+		temp.matrix[1][1] = sy;
+		temp.matrix[2][2] = sz;
+		//transform = transform * temp
+		transform.multiply(transform, temp);
+	}
+	
+	//applies the transformation to this point
+	public Point applyToPoint(Point src, Point a) {
+		return this.transform.multiply(src, a);
+	}
+	
+	public void apply() {
+		//finalizes this matrix - by finding the inverse matrix!
 		
 	}
 	public void reset() {
+		transform.setIdentity();
+	}
+	
+	public static void main(String[] args) {
+		System.out.println("Testing transformations");
 		
+		Point p = new Point(4, 3, 3);
+		Point ans = new Point();
+		
+		Transformation trans = new Transformation();
+		trans.translate(3, 4, 5);
+		System.out.println("Translate");
+		System.out.println(trans.transform);
+		trans.applyToPoint(ans, p);
+		System.out.println(ans);
+		
+		
+		trans.reset();
+		trans.scale(5, 6, 7);
+		System.out.println("Scale");
+		System.out.println(trans.transform);
+		trans.applyToPoint(ans, p);
+		System.out.println(ans);
+		
+		trans.reset();
+		trans.rotate(45, 0, 0);
+		System.out.println("Rotate");
+		System.out.println(trans.transform);
+		trans.applyToPoint(ans, p);
+		System.out.println(ans);
 	}
 }
